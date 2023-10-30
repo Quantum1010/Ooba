58dec45d9f3b6e4cbc34700278ad0cff6b9b62d0
Quantum1010 <135439556+quantum1010@users.noreply.github.com>
2023-10-30 17:25:43 +0900

    わかんない

--- GPT_test.py
+++ GPT_test.py
@@ -1,9 +1,15 @@
+import json
+
 import asyncio
-from EdgeGPT import Chatbot, ConversationStyle
+from EdgeGPT.EdgeGPT import Chatbot, ConversationStyle
+from os import getenv
 
 async def main():
-    bot = await Chatbot.create()
-    print(await bot.ask(prompt="大阪府の人口について教えてください。", conversation_style=ConversationStyle.creative))
+    bot = await Chatbot.create(cookies=getenv("EDGE_COOKIES"))
+    print(await bot.ask(
+        prompt="大阪府の人口について教えてください。",
+        conversation_style=ConversationStyle.creative,
+        simplify_response=True,))
     await bot.close()
 
 if __name__ == "__main__":
--- 自動操作.py
+++ 自動操作.py
@@ -1,7 +1,9 @@
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 driver = webdriver.Chrome()
 
 driver.get('https://www.bing.com/search?q=Bing+AI&showconv=1&FORM=hpcodx')
-form = driver.find_element(By.XPATH,'//*[@id="cib-action-bar-main"]//div/div[2]/div/div[1]/cib-text-input')
+time.sleep(10)
+form = driver.find_element(By.XPATH,'/html/body/div[1]/cib-serp//cib-action-bar//div/div[2]/div/div[1]/cib-text-input//form/label/data-input')
 form.send_keys('大場くんの好きな食べ物はなんですか？')
